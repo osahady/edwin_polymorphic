f5da9a425959bb58ce5a260240f5630502584816
Osahady <osahady@gmail.com>
2019-10-16 11:04:37 +0300

    finish

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Photo;
+use App\Staff;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +19,42 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/create', function(){
+    $staff = Staff::find(2);
+    $staff->photos()->create(['path_image'=>'example.jpg']);
+});
+
+Route::get('/read', function(){
+    $staff = Staff::findOrFail(1);
+    foreach ($staff->photos as $photo) {
+        return $photo->path_image;
+    }
+});
+
+Route::get('/update', function(){
+    $staff = Staff::find(1);
+    $photo = $staff->photos()->whereId(1)->first();
+    $photo->path_image = "updated example.jpg";
+    $photo->save();
+});
+
+Route::get('/delete', function(){
+    $staff = Staff::findOrFail(1);
+    $staff->photos()->whereId(1)->delete();
+});
+
+Route::get('/assign', function(){
+    $staff = Staff::findOrFail(1);
+    $photo = Photo::findOrFail(4);
+    $staff->photos()->save($photo);
+
+});
+
+Route::get('unassign', function(){
+    $staff = Staff::findOrFail(1);
+    $photo = Photo::findOrFail(4);
+    $staff->photos()->save($photo);
+});
 
